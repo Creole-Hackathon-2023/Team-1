3080592b471b567c0a8587d3fccc899586cf59bd
Ganesh Tak <ganesh.tak@creolestudios.com>
2023-02-04 15:02:12 +0530

    ADDED auth ascreen ui

---
+++ src/components/home/VenueListing.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function VenueListing() {
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/containers/DetailPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function DetailPage() {
+  return (
+    <div>
+      <div className="illustration-wrapper">
+        <img
+          src="https://mixkit.imgix.net/art/preview/mixkit-left-handed-man-sitting-at-a-table-writing-in-a-notebook-27-original-large.png?q=80&auto=format%2Ccompress&h=700"
+          alt="Login"
+        />
+      </div>
+    </div>
+  );
+}
